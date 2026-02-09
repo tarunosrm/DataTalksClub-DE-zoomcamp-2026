92eabf6e14a07bab9620a0b89448f4a569237085
tarunosrm <155843906+tarunosrm@users.noreply.github.com>
2026-02-09 22:41:13 +0530

    Q3 BigQuery columnar storage
    Added SQL queries to process yellow trip data.

---
+++ hw3/q3_columnar_storage.sql
@@ -0,0 +1,8 @@
+
+--process 155.12MB data
+SELECT PULocationID
+FROM `taxi-rides-ny-bq.dezoomcamp_hw3_bq.yellow_tripdata_2024`;
+
+--process 310.24 MB data
+SELECT PULocationID, DOLocationID
+FROM `taxi-rides-ny-bq.dezoomcamp_hw3_bq.yellow_tripdata_2024`;
